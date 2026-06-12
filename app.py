cc0e52f08a5c867b6830c45f5cd2edc64692f394
yuvrajbhati649-prog <yuvrajbhati649@gmail.com>
2026-06-12 22:28:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-
+from flask import Flask,request,jsonify
+import joblib
+import numpy as np
+from flask import Response
+app=Flask(__name__)
+#load the model once
+model=joblib.load("rf_model.pk1")
+@app.route('/predict',methods=['POST'])
+def predict() -> Response:
+    try:
+        data=request.get_json()
+        input_data=np.array(data['input']).reshape(1,-1)
+        prediction=model.predict(input_data)
+        return jsonify({'prediction':int(prediction[0])})
+    except Exception as o:
+        return jsonify({'error':str(0)})
+    
+if __name__=='__main__':
+    app.run(debug=True)
