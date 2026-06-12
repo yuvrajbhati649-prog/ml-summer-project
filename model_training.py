dbd5c0cdfaf4bc7d2b95985b5900080c8ae5cae9
yuvrajbhati649-prog <yuvrajbhati649@gmail.com>
2026-06-12 22:31:45 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,1 +1,18 @@
+import numpy as np
+import pandas as pd
 
+
+df=pd.read_csv("Social_Network_Ads.csv")
+print(df.head(2))
+df=df.drop(columns=["User ID","Gender"])
+x=df.drop(columns=["Purchased"])
+y=df["Purchased"]
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state=42)
+from sklearn.ensemble import RandomForestClassifier
+rf= RandomForestClassifier()
+rf.fit(x_train,y_train)
+import joblib 
+#save the model
+joblib.dump(rf,"rf_model.pk1")
+print("Model saved as rf_model.pk1")
