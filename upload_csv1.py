b7550da6b746d9c830d57c81608edf8f028aa3df
yuvrajbhati649-prog <yuvrajbhati649@gmail.com>
2026-06-12 22:34:49 +0530

    Update upload_csv1.py

--- upload_csv1.py
+++ upload_csv1.py
@@ -1,1 +1,39 @@
+import pandas as pd
+import mysql.connector
 
+# CSV Read
+df =pd.read_csv("Social_Network_Ads.csv")
+
+# MySQL Connection
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="pammysaini",   # apna password likho
+    database="social_network"
+)
+
+cursor = conn.cursor()
+
+for _, row in df.iterrows():
+    sql = """
+    INSERT INTO social_network_ads
+    (User_ID, Gender, Age, EstimatedSalary, Purchased)
+    VALUES (%s, %s, %s, %s, %s)
+    """
+
+    values = (
+        int(row["User ID"]),
+        row["Gender"],
+        int(row["Age"]),
+        int(row["EstimatedSalary"]),
+        int(row["Purchased"])
+    )
+
+    cursor.execute(sql, values)
+
+conn.commit()
+
+print("Data inserted successfully!")
+
+cursor.close()
+conn.close()
